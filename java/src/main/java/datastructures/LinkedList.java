d2f7e31fc69515c0c68c7ab73de3c30305396b04
André Luis Gomes <andre.gomes@arquivei.com.br>
2020-04-02 09:32:51 -0300

    Java LinkedList implementation

---
+++ java/src/main/java/datastructures/LinkedList.java
@@ -0,0 +1,148 @@
+package datastructures;
+
+public class LinkedList<T> {
+
+    private int size = 0;
+    private Node<T> head;
+
+    public void addFront(final T data) {
+        //create a new new Node
+        var newNode = new Node<T>(data);
+
+        if (head != null) {
+            //Set new new to head then pointer to older head
+            newNode.next = this.head;
+
+            //Current head, point to new node
+        }
+        this.head = newNode;
+        increaseSize();
+    }
+
+    public void addBack(T data) {
+        var newTail = new Node<T>(data);
+
+        if (head == null){
+            head = newTail;
+        }else {
+            //Set new TAIL
+            toTail().next = newTail;
+        }
+        increaseSize();
+    }
+
+    public T getFirst() {
+        if(this.head == null){
+            throw new IllegalStateException("Head is NULL");
+        }
+        return this.head.data;
+    }
+
+    /**
+     * Get last is same as Get Tail
+     *
+     * @return Tail
+     */
+    public T getLast() {
+        if(this.head == null){
+            throw new IllegalStateException("Head is NULL");
+        }
+
+        return toTail().data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Size by brute force
+     *
+     * @return
+     */
+    public int count() {
+        if (head == null) {
+            return 0;
+        }else {
+            int count = 1;
+            var current = head;
+
+            //while we are not at TAIL
+            while (current.next != null) {
+                current = current.next;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public void clear() {
+        this.size = 0;
+        this.head = null; //let the garbage collector work. Clean all nodes
+    }
+
+    public void delete(T value) {
+        if (head == null) {
+            return;
+        }
+        var current = head;
+        var removed = false;
+
+        if (value.equals(head.data)){
+            head = head.next;
+            removed = true;
+        } else {
+
+            //while we are not at TAIL
+            while (current.next != null) {
+                current = current.next;
+                if (current.data.equals(value)) {
+                    current = current.next.next;
+                    removed = true;
+                    break;
+                }
+
+            }
+        }
+        decreaseSizeIf(removed);
+    }
+
+    /**
+     * Start at the Head
+     *
+     * Walking down to the end
+     *
+     * @return tail
+     */
+    private Node<T> toTail() {
+        var current = head;
+
+        //while we are not at TAIL
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    private void increaseSize() {
+        this.size++;
+    }
+
+    private void decreaseSizeIf(final boolean removed) {
+        if (removed) {
+            this.size--;
+        }
+    }
+
+
+    private static class Node<T> {
+
+        T data;
+        Node<T> next;
+
+        public Node(final T data) {
+            this.data = data;
+        }
+    }
+}
---
+++ java/src/test/java/datastructures/LinkedListTest.java
@@ -0,0 +1,85 @@
+package datastructures;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    @Test
+    public void shouldAddFrontOfList() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        assertThat(linkedList.getFirst(), is(3));
+        assertThat(linkedList.getLast(), is(1));
+    }
+
+    @Test
+    public void shouldAddBackOfList() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.addBack(1);
+
+        assertThat(linkedList.getFirst(), is(1));
+        assertThat(linkedList.getLast(), is(1));
+    }
+
+    @Test
+    public void shouldGetSize() {
+        var linkedList = new LinkedList<String>();
+        assertThat(linkedList.size(), is(0));
+        assertThat(linkedList.count(), is(0));
+
+        linkedList.addFront("add1");
+        assertThat(linkedList.size(), is(1));
+        assertThat(linkedList.count(), is(1));
+
+        linkedList.addFront("add2");
+        assertThat(linkedList.size(), is(2));
+        assertThat(linkedList.count(), is(2));
+    }
+
+    @Test
+    public void shouldClearTheList() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        linkedList.clear();
+
+        assertThat(linkedList.size(), is(0));
+    }
+
+    @Test
+    public void shouldDeleteHeadFromListByValue() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        linkedList.delete(3);
+
+        assertThat(linkedList.size(), is(2));
+        assertThat(linkedList.getFirst(), is(2));
+        assertThat(linkedList.getLast(), is(1));
+    }
+
+    @Test
+    public void shouldDeleteFromListByValue() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        linkedList.delete(2);
+
+        assertThat(linkedList.size(), is(2));
+        assertThat(linkedList.getFirst(), is(3));
+        assertThat(linkedList.getLast(), is(1));
+    }
+
+}
